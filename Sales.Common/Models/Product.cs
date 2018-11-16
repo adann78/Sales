8e4c430f0bf0cfb93fca6894521de037c5107c6d
adan_ilich <adan_ilich@hotmail.com>
2018-11-16 16:30:17 -0700

    controlador

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Sales.Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public bool IsAvialable { get; set; }
+        public DateTime PublishOn { get; set; }
+    }
+}
